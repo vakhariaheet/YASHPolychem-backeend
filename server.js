d086c059d326711e5c9789b3243414d152dc11a8
Heet Vakharia <heet1476@gmail.com>
2021-05-26 18:21:16 +0530

    increased Upload size

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const updateDealers = require('./Routes/updateDealers.routes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
